4f5fed6faaaa5be6272641e0222de6d1dc67dbbd
refeahcs00 <owens0506@gmail.com>
2022-12-14 22:42:25 -0500

    rendered order list

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+const players = [
+  {name: 'Owen Schaefer', dob: '05-06-1998', sex: 'male', score: 89},
+  {name: 'Bobby Piccerillo', dob: '10-22-1997', sex: 'male', score: 54},
+  {name: 'Nick Zuchelli', dob: '10-22-1997', sex: 'male', score: 62},
+  {name: 'MaryKate Robinson', dob: '01-24-1996', sex: 'female', score: 32},
+]
+
 class Leaderboard extends React.Component {
   render() {
+    const sorted_board = this.props.board.sort((a, b) => b.score - a.score);
+    const jsx_board = sorted_board.map((sorted_board) =>
+      <li>{sorted_board.name} - {sorted_board.score}</li>
+    );
+
     return (
       <div>
         <h1>Leaderboard</h1>
+        <ol>{jsx_board}</ol>
       </div>
     );
   }
@@ -29,7 +42,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <Leaderboard />
+      <Leaderboard board={players} />
     </div>
   );
 }
