887004dc96e933e86f3c2a2653fcd10ed5bacb9f
refeahcs00 <owens0506@gmail.com>
2022-12-24 01:30:22 -0500

    added top scoreboard

--- pick5/app/views.py
+++ pick5/app/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'app/index.html', context={})
+    game_list = [
+        {
+            'away': {'team': 'Steelers', 'score': 24},
+            'home': {'team': 'Raiders', 'score': 21},
+        },
+        {
+            'away': {'team': 'Jaguars', 'score': 19},
+            'home': {'team': 'Jets', 'score': 3},
+        },
+    ]
+
+    return render(request, 'app/index.html', context={'game_list': game_list})
+    # return render(request, 'app/index.html', context={'game_list': None})
