d5318dd31f1954772bde93d8a1fcf4ae44c5b3a1
refeahcs00 <owens0506@gmail.com>
2022-12-25 17:21:53 -0500

    Added basic login page

--- pick5/app/urls.py
+++ pick5/app/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view()),
     path('', views.index, name='index'),
 ]
--- pick5/app/views.py
+++ pick5/app/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def login(request):
+    username = request.POST.get('username')
+    print(f'{username}')
+    password = request.POST.get('password')
+    print(f'{password}')
+    user = authenticate(request=request, username=username, password=password)
+    if user:
+        login(request, user)
+
+        return redirect('/')
+    else:
+        print('Unable to log in user')
+
+    return render(request, 'registration/login.html', context={})
+
+
+@login_required
 def index(request):
     game_list = [
         {
@@ -14,4 +33,3 @@ def index(request):
     ]
 
     return render(request, 'app/index.html', context={'game_list': game_list})
-    # return render(request, 'app/index.html', context={'game_list': None})
