91ee47e05d099918eb3d9d2a9497d6470fb02ac4
refeahcs00 <owens0506@gmail.com>
2023-01-16 21:47:07 -0500

    updated model structure

--- pick5/app/admin.py
+++ pick5/app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from app.models import Game, Team
+from app.models import Team
 
 # Register your models here.
-admin.register(Game)
 admin.register(Team)
--- pick5/app/models.py
+++ pick5/app/models.py
@@ -1,22 +1,39 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Team(models.Model):
     abbreviation = models.CharField(max_length=3, null=False, blank=False, unique=True)
     name = models.CharField(max_length=50, null=False, blank=False, unique=True)
-    active = models.BooleanField(null=False, blank=True, default=True)
-
-    def __str__(self):
-        return self.abbreviation
 
 
-class Game(models.Model):
-    WEEK_CHOICES = [(x, f'Week {x}') for x in range(1, 19)]
+class Lines(models.Model):
+    home_spread = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=1)
+    away_spread = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=1)
+    over = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=1)
+    under = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=1)
 
-    game_id = models.IntegerField(null=False, blank=True)
-    week = models.CharField(max_length=8, null=False, blank=False, choices=WEEK_CHOICES)
-    spread = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=1)
-    team = models.ForeignKey(to='Team', null=False, blank=False, on_delete=models.DO_NOTHING)
 
-    def __str__(self):
-        return f'{self.week}: {self.team.abbreviation} ({self.spread})'
+class Match(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    away_team = models.ForeignKey(to=Team, related_name='+', on_delete=models.DO_NOTHING)
+    home_team = models.ForeignKey(to=Team, related_name='+', on_delete=models.DO_NOTHING)
+    lines = models.ForeignKey(to=Lines, on_delete=models.DO_NOTHING)
+    away_score = models.IntegerField(null=False, blank=False, default=0)
+    home_score = models.IntegerField(null=False, blank=False, default=0)
+
+
+class Season(models.Model):
+    start = models.DateField(null=False)
+    end = models.DateField(null=False)
+
+
+class Pick(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    team = models.ForeignKey(to=Team, on_delete=models.DO_NOTHING)
+    match = models.ForeignKey(to=Match, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING)
+    season = models.ForeignKey(to=Season, on_delete=models.CASCADE)
+    week = models.IntegerField(null=False, blank=False, choices=[(i, f'Week {i}') for i in range(1, 19)])
